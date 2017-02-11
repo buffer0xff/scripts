f4d5e01c206f4cb79c7de04ddf6b1be8ee3c8687
Sky <buffer0xff@gmail.com>
2017-02-11 22:44:45 +0800

    Update chproxy.sh

--- chproxy.sh
+++ chproxy.sh
@@ -2,17 +2,30 @@
 
 config_directory=/etc/shadowsocks
 file_name_array=()
+file_name_array=()
+
+current_proxy=`ps -ef | grep -io  '/etc/shadowsocks/.*.json' | grep -v '*' | cut -d"/" -f 4 | cut -d"." -f1`
 
 i=0
-echo ""
+echo '
+#########################
+# Please select a proxy # 
+#########################
+'
 for file_name in ${config_directory}/*; do
     file_name_array[$i]=$file_name
-    echo [$i]$file_name
+    pure_name=`echo $file_name | cut -d "/" -f4 | cut -d "." -f1` 
+    if [ $pure_name == $current_proxy ]; then
+        echo "[$i] $pure_name        <-- current" 
+    else
+        echo [$i] $pure_name
+    fi
     let i=$i+1
 done
 
-echo ""
-echo -n "[*] Please select a config file: "
+echo -n "
+[*] Input Number:" 
+
 read index
 echo ${file_name_array[$index]}
 sudo sslocal -c ${file_name_array[$index]} -d restart
